53a32e4772e12b13507bcbdaf9dc3b956605cab3
kobaltgit <vash_email@example.com>
2026-09-05 21:46:55 +0300

    feat: add AppConstants class for application metadata and project links

--- website/lib/constants.dart
+++ website/lib/constants.dart
@@ -2,11 +2,13 @@ class AppConstants {
   static const String appName = 'Peekit';
   static const String appVersion = 'v1.0.0';
   static const String githubUrl = 'https://github.com/kobaltgit/peekit';
-  static const String downloadExeUrl = 'https://github.com/kobaltgit/peekit/releases/latest';
-  static const String downloadZipUrl = 'https://github.com/kobaltgit/peekit/releases/latest';
+  static const String downloadExeUrl =
+      'https://github.com/kobaltgit/peekit/releases/latest';
+  static const String downloadZipUrl =
+      'https://github.com/kobaltgit/peekit/releases/latest';
 
   // Sibling ecosystem projects
   static const String minibinUrl = 'https://kobaltgit.github.io/minibin/';
-  static const String undoitUrl = 'https://kobaltgit.github.io/undoit/';
+  static const String undoitUrl = 'https://kobaltgit.github.io/Undoit/';
   static const String polyShiftUrl = 'https://kobaltgit.github.io/polyshift/';
 }
